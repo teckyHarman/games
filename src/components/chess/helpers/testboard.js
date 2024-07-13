59ab40b53771f177ab4bf6d03f5ab91b70af0fbb
SudhansuSekharSahoo18 <sudhansusahoo359@gmail.com>
2024-07-13 13:16:04 +0530

    code refactor for testboard

--- src/components/chess/helpers/testboard.js
+++ src/components/chess/helpers/testboard.js
@@ -3,18 +3,15 @@ import {
     blackRook, blackKnight, blackBishop, blackKing, blackQueen, blackPawn
   } from './constants.js'
 
-const whitePieces = [whiteRook, whiteKnight, whiteBishop, whiteKing, whiteQueen, whitePawn]
-const blackPieces = [blackRook, blackKnight, blackBishop, blackKing, blackQueen, blackPawn]
-
 export const startBoard = [
-    [blackPieces[0], blackPieces[1], blackPieces[2], blackPieces[4], blackPieces[3], blackPieces[2], blackPieces[1], blackPieces[0]],
-    [blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5]],
+    [blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackBishop, blackKnight, blackRook],
+    [blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
-    [whitePieces[5], whitePieces[5], whitePieces[5], whitePieces[5], whitePieces[5], whitePieces[5], whitePieces[5], whitePieces[5]],
-    [whitePieces[0], whitePieces[1], whitePieces[2], whitePieces[4], whitePieces[3], whitePieces[2], whitePieces[1], whitePieces[0]],
+    [whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn, whitePawn],
+    [whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whiteBishop, whiteKnight, whiteRook],
   ];
 
 export const pawnTestBoard = [
@@ -56,7 +53,7 @@ export const kingBoard =
     [
       ['', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', ''],
-      ['', '', '', '', whiteKing, '', '', ''],
+      [blackRook, '', '', whitePawn, whiteKing, '', '', ''],
       ['', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', ''],
       ['', '', blackKing, '', '', '', '', ''],
@@ -66,25 +63,25 @@ export const kingBoard =
 
     
 export const enpassantBoard = [
-  [blackPieces[0], blackPieces[1], blackPieces[2], blackPieces[4], blackPieces[3], blackPieces[2], blackPieces[1], blackPieces[0]],
-  [blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5]],
+  [blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackBishop, blackKnight, blackRook],
+  [blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn],
   ['', '', '', '', '', '', '', ''],
   ['', '', '', '', whitePawn, '', '', ''],
   ['', '', '', blackPawn, '', '', '', ''],
   ['', '', '', '', '', '', '', ''],
-  [whitePieces[5], whitePieces[5], whitePieces[5], whitePieces[5], '', whitePieces[5], whitePieces[5], whitePieces[5]],
-  [whitePieces[0], whitePieces[1], whitePieces[2], whitePieces[4], whitePieces[3], whitePieces[2], whitePieces[1], whitePieces[0]],
+  [whitePawn, whitePawn, whitePawn, whitePawn, '', whitePawn, whitePawn, whitePawn],
+  [whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whiteBishop, whiteKnight, whiteRook],
 ];
 
     
 export const castlingBoard = [
-  [blackPieces[0], '', '', '', blackPieces[3], '', '', blackPieces[0]],
-  [blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5], blackPieces[5]],
+  [blackRook, '', '', '', blackKing, '', '', blackRook],
+  [blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn],
   ['', '', '', '', '', '', '', ''],
   ['', '', '', '', '', '', '', ''],
   ['', '', '', '', whitePawn, '', '', ''],
   ['', '', '', '', '', '', '', ''],
-  [whitePieces[5], whitePieces[5], whitePieces[5], whitePieces[5], '', whitePieces[5], whitePieces[5], whitePieces[5]],
-  [whitePieces[0], '', '', '', whitePieces[3], '', '', whitePieces[0]],
+  [whitePawn, whitePawn, whitePawn, whitePawn, '', whitePawn, whitePawn, whitePawn],
+  [whiteRook, '', '', '', whiteKing, '', '', whiteRook],
 ];
 
