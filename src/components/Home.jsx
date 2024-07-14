ef66a2fe49505d31bdfb4a03dabe7ef4dacede02
SudhansuSekharSahoo18 <sudhansusahoo359@gmail.com>
2024-07-14 18:45:47 +0530

    added navigation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,36 +5,46 @@ import LudoGame from './ludo/Ludo';
 import BingoGame from './bingo/BingoGame';
 import Chess from './chess/Chess';
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function Home() {
   const [screen, setScreen] = useState()
-
-  const onClick = (src) => {
-
-  }
+  const [selectedTab, setSelectedTab] = useState('')
 
   return (
-    <>
-        <h1>Home Screen</h1>
-        <div className="screen">
-          <button onClick={()=>onClick('TicTacToe')}>TicTacToe</button>
-            <div className='container' >
-              <TicTacToe />
-            </div>
-            {/* <div className='container' >
-              <MemoryGame />
-            </div> */}
-            {/* <div className='container' >
-              <LudoGame />
-            </div>
-            <div className='container' >
-              <BingoGame />
-            </div> */}
-            <div className='container' >
-              <Chess />
-            </div>
+    <Router>
+      <div className="screen">
+        <nav className="nav">
+          <ul>
+            <li className={selectedTab === 'pos' ? 'selectedNav' : ''}>
+              <Link to="/TicTacToe">TicTacToe</Link>
+            </li>
+            <li className={selectedTab === 'bills' ? 'selectedNav' : ''}>
+              <Link to="/MemoryGame">MemoryGame</Link>
+            </li>
+            <li className={selectedTab === 'generateBarcode' ? 'selectedNav' : ''}>
+              <Link to="/LudoGame">LudoGame</Link>
+            </li>
+            <li className={selectedTab === 'saleReport' ? 'selectedNav' : ''}>
+              <Link to="/BingoGame">BingoGame</Link>
+            </li>
+            <li className={selectedTab === 'saleReport' ? 'selectedNav' : ''}>
+              <Link to="/Chess">Chess</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className='container' >
+          <Routes>
+            <Route path="/" element={<Chess />} />
+            <Route path="/TicTacToe" element={<TicTacToe />} />
+            <Route path="/MemoryGame" element={<MemoryGame />} />
+            <Route path="/LudoGame" element={<LudoGame />} />
+            <Route path="/BingoGame" element={<BingoGame />} />
+            <Route path="/Chess" element={<Chess />} />
+          </Routes>
         </div>
-    </>
+      </div>
+    </Router>
   );
 }
 
